e200d00f280fe865775a7bd1b4a86816d407e2b3
Student <student@d17-429-pc-17.ipa.ii.agh.edu.pl>
2025-04-27 09:32:14 +0200

    All meeting gathering is added

---
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -0,0 +1,28 @@
+package com.company.enroller.controllers;
+
+import java.util.Collection;
+
+import com.company.enroller.model.Meeting;
+import com.company.enroller.persistence.MeetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+@RestController
+@RequestMapping("/meetings")
+public class MeetingRestController {
+
+    @Autowired
+    MeetingService meetingService;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeetings() {
+        Collection<Meeting> meetings = meetingService.getAll();
+        return new ResponseEntity<>(meetings, HttpStatus.OK);
+    }
+
+}
